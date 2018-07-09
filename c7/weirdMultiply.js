f58c5e6d8346fa4154d4add180ce751583c0c468
YudhistiraM <ma.yudhistira@gmail.com>
2018-07-09 16:22:27 +0700

    C7 Done

---
+++ c7/weirdMultiply.js
@@ -0,0 +1,18 @@
+function weirdMultiply(sentence){
+  let hasil = 1;
+  let strAngka = sentence.toString();
+
+  if (strAngka.length == 1) {
+    hasil = parseInt(strAngka);
+    return hasil;
+  } else {
+    for (let i = 0; i < strAngka.length; i++) {
+      hasil *= parseInt((strAngka.charAt(i)));
+    }
+    return weirdMultiply(hasil);
+  }
+
+}
+console.log(weirdMultiply(39));
+console.log(weirdMultiply(999));
+console.log(weirdMultiply(3));
