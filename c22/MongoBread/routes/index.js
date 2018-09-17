f6f0d63af8c8ee8b3130af93c8c5aae7d02a62a3
YudhistiraM <ma.yudhistira@gmail.com>
2018-09-17 13:38:08 +0700

    c22 done

--- c22/MongoBread/routes/index.js
+++ c22/MongoBread/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var moment = require('moment');
+var objectId = require('mongodb').ObjectId;
 
 module.exports = function(data){
 
@@ -10,32 +11,43 @@ module.exports = function(data){
     let limit = 3;
     let offset = (currentPage - 1) * limit;
 
-    // let sql = `SELECT Bread_id, string, integer, float, date, boolean FROM detailBread`;
-    // let sql = `SELECT count(*) as total FROM data`;
-    // Search (tidak perlu membuat path route baru karena halaman masih sama dengan path ini)
     let params = {};
-    if(req.query.cid && req.query.Bread_id){
-      params._id = req.query.Bread_id;
+    if(req.query.cid && req.query._id){
+      params._id = objectId(req.query._id);
     }
+
     if(req.query.cstring && req.query.string){
-      params.stringdata = req.query.string
+      // db.data.find({"stringdata": {$regex: "ac", $options: "$i"}});
+      // db.data.find({"stringdata": /Achmad angri/i});
+      params.stringdata = {$regex: req.query.string, $options: "$i"}
     }
+
     if(req.query.cinteger && req.query.integer){
-      params.integerdata = parseInt(req.query.integer)
+      params.integerdata = req.query.integer
+      //parseInt() --> dihilangkan
     }
+
     if(req.query.cfloat && req.query.float){
-      params.floatdata = parseFloat(req.query.float)
+      params.floatdata = req.query.float
+      //parseFloat() --> dihilangkan
     }
+
     if(req.query.cdate && req.query.sdate && req.query.edate ){
-      //
+      // db.data.find({$and : [{datedata: {$gt: "2018-03-13"}}, {datedata: {$lt: "2018-09-17"}}]});
+      params.datedata = { $gte: req.query.sdate, $lte: req.query.edate }
     }
+
     if(req.query.cboolean && req.query.boolean){
-      params.booleandata = JSON.parse(req.query.boolean)
+      params.booleandata = req.query.boolean
+      // JSON.parse() --> dihilagkan
     }
 
     data.find(params).count(function(err, total){
+      //console.log(params);
       let pages = Math.ceil(total / limit);
       data.find(params).limit(limit).skip(offset).toArray(function(err, docs){
+        console.log(params);
+        // console.log(docs);
         res.render('list', {rows: docs, pagination: {total, pages, currentPage, limit, offset, url: url}, query: req.query, moment: moment});
       })
     })
@@ -61,7 +73,6 @@ module.exports = function(data){
       var myobj = {stringdata: `${string}`, integerdata: `${integer}`, floatdata: `${float}`, datedata: `${date}`, booleandata: `${boolean}`};
       dbo.collection("data").insertOne(myobj, function(err, res) {
         if (err) throw err;
-
         // console.log(res);
       });
     });
@@ -71,71 +82,59 @@ module.exports = function(data){
   //Get Fungsi Delete
   router.get('/delete/:_id', function(req, res) {
     let B_id = req.params._id;
-    const removeDocument = function(db, callback) {
-      // Get the documents collection
-      const collection = db.collection('data');
-      var myquery = {"_id": `ObjectId("${B_id}")`};
-      // Delete document where a is 3
-      collection.deleteOne(myquery, function(err, result) {
-        // assert.equal(err, null);
-        // assert.equal(1, result.result.n);
-        console.log("Removed the document with the field a equal to 3");
-        callback(result);
-      });
-    }
-    // var MongoClient = require('mongodb').MongoClient;
-    // var url = "mongodb://localhost:27017/";
-    //
-    // MongoClient.connect(url, function(err, db) {
-    //   if (err) throw err;
-    //   var dbo = db.db("cruddb");
-    //   var myquery = {"_id": `ObjectId("${B_id}")`};
-    //   dbo.collection("data").deleteOne(myquery, function(err, obj) {
-    //     if (err) throw err;
-    //     console.log(myquery);
-    //     res.redirect('/');
-    //   });
-    // });
-    // let sql = `DELETE FROM data WHERE id = ${B_id}`;
-    // pool.query(sql, function (err){
-    //   if (err) {
-    //     throw err;
-    //   }
 
-    // })
+    var MongoClient = require('mongodb').MongoClient;
+    var url = "mongodb://localhost:27017/";
+
+    MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db("cruddb");
+      // var myquery = {"_id": objectId(B_id)};
+      dbo.collection("data").deleteOne({"_id": objectId(B_id)}, function(err, obj) {
+        if (err) throw err;
+        // console.log(myquery);
+        res.redirect('/');
+      });
+    });
   });
 
-  //
-  //   //Get Halaman Edit
-  //   router.get('/edit/:id', function(req, res, next) {
-  //     let B_id = req.params.id;
-  //     let sql = `SELECT * FROM data`;
-  //     pool.query(sql, [], (err, data) =>{
-  //       if (err) {
-  //         throw err;
-  //       }
-  //       // console.log(data);
-  //       let item = data.rows.filter(x=>x.id == B_id);
-  //       console.log(item);
-  //       res.render('edit', {item: item, moment: moment});
-  //     })
-  //   });
-  //
-  //   router.post('/edit/:id', function(req, res) {
-  //     let B_id = req.params.id;
-  //     let string = req.body.string;
-  //     let integer = req.body.integer;
-  //     let float = req.body.float;
-  //     let date = req.body.date;
-  //     let boolean = req.body.boolean;
-  //     let sql = `UPDATE data SET stringdata = '${string}', integerdata = ${integer}, floatdata = ${float}, datedata = '${date}', booleandata = '${boolean}' WHERE id = '${B_id}'`;
-  //     pool.query(sql, function(err,rows) {
-  //       if (err) {
-  //         throw err;
-  //       }
-  //     })
-  //     res.redirect('/');
-  //   });
+  //Get Halaman Edit
+  router.get('/edit/:_id', function(req, res, next) {
+    let B_id = req.params._id;
+    data.find(objectId(B_id)).toArray(function(err, docs){
+
+      res.render('edit', {item: docs});
+      // console.log(docs);
+    })
+  });
+
+  router.post('/edit/:_id', function(req, res) {
+    let B_id = req.params._id;
+    let item =
+    {
+      stringdata : req.body.string,
+      integerdata : req.body.integer,
+      floatdata : req.body.float,
+      datedata : req.body.date,
+      booleandata : req.body.boolean
+    };
+
+    var MongoClient = require('mongodb').MongoClient;
+    var url = "mongodb://127.0.0.1:27017/";
+
+    MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db("cruddb");
+      var myquery = {"_id": objectId(B_id)};
+      var newvalues = { $set: item };
+      dbo.collection("data").updateOne(myquery, newvalues, function(err, res) {
+        if (err) throw err;
+      });
+    });
+
+
+    res.redirect('/');
+  });
 
   return router;
 }
